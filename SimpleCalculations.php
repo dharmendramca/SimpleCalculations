b7a474c19ab3662deb03a8e09691b9d82abd336c
dharmendramca <53340783+dharmendramca@users.noreply.github.com>
2019-09-30 09:37:33 +0530

    Add files via upload

---
+++ SimpleCalculations.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+*************************************************
+*************************************************
+Author: Dharmendra Kumar
+
+Description: Simple Calculations is a PHP class that provide some usefull functions. 
+
+*************************************************
+*/
+class SimpleCalculations{
+
+public $x;
+public $y;
+
+function SimpleAdd($x,$y){
+
+return ($x+$y);
+
+}
+
+function SimpleMin($x,$y){
+
+return ($x-$y);
+
+}
+
+function SimpleMul($x,$y){
+
+return ($x*$y);
+
+}
+
+function SimpleDiv($x,$y){
+
+return ($x/$y);
+
+}
+
+
+}
